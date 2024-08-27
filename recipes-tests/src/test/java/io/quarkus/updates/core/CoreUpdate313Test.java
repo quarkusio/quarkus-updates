15237de0ba214c7dc0cd593ebd8366678cd8b9de
Guillaume Smet <guillaume.smet@gmail.com>
2024-08-27 16:27:13 +0200

    Add recipe to rewrite OIDC test server URL
    Related to https://github.com/quarkusio/quarkus/wiki/Migration-Guide-3.13#dev-services-startup-detection-change

--- recipes-tests/src/test/java/io/quarkus/updates/core/CoreUpdate313Test.java
+++ recipes-tests/src/test/java/io/quarkus/updates/core/CoreUpdate313Test.java
@@ -1,9 +1,11 @@
 package io.quarkus.updates.core;
 
 import static org.openrewrite.java.Assertions.java;
+import static org.openrewrite.properties.Assertions.properties;
 
 import java.nio.file.Path;
 
+import org.intellij.lang.annotations.Language;
 import org.junit.jupiter.api.Test;
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
@@ -106,5 +108,34 @@ public class CoreUpdate313Test implements RewriteTest {
                 class GitHubAppTest {
                 }
             """));
+    }
+
+    @Test
+    void testUpdateTestOIDCAuthServerUrl() {
+        @Language("properties")
+        String originalProperties = """
+            %test.quarkus.oidc.auth-server-url=${keycloak.url}/realms/quarkus/
+            """;
+
+        @Language("properties")
+        String afterProperties = """
+            %test.quarkus.oidc.auth-server-url=${keycloak.url:replaced-by-test-resource}/realms/quarkus/
+            """;
+
+        rewriteRun(properties(originalProperties, afterProperties, spec -> spec.path("src/main/resources/application.properties")));
+
+        @Language("properties")
+        String unchangedWrongProfileOriginalProperties = """
+            quarkus.oidc.auth-server-url=${keycloak.url}/realms/quarkus/
+            """;
+
+        rewriteRun(properties(unchangedWrongProfileOriginalProperties, spec -> spec.path("src/main/resources/application.properties")));
+
+        @Language("properties")
+        String unchangedWrongValueOriginalProperties = """
+            %test.quarkus.oidc.auth-server-url=another-value
+            """;
+
+        rewriteRun(properties(unchangedWrongValueOriginalProperties, spec -> spec.path("src/main/resources/application.properties")));
     }
 }
