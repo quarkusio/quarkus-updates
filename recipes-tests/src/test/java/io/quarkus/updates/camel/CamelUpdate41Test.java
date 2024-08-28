fe60e2e493b9de27ebe2e1377c3442a1d824e56a
JiriOndrusek <ondrusek.jiri@gmail.com>
2024-08-28 13:09:08 +0200

    Proper test environment for the camel-quarkus recipes

--- recipes-tests/src/test/java/io/quarkus/updates/camel/CamelUpdate41Test.java
+++ recipes-tests/src/test/java/io/quarkus/updates/camel/CamelUpdate41Test.java
@@ -1,4 +1,16 @@
 package io.quarkus.updates.camel;
 
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.TypeValidation;
+
 public class CamelUpdate41Test extends org.apache.camel.updates.camel44.CamelUpdate41Test {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        //let the parser be initialized in the camel parent
+        super.defaults(spec);
+        //recipe has to be loaded differently
+        CamelQuarkusTestUtil.recipe3_8(spec)
+                .typeValidationOptions(TypeValidation.none());
+    }
 }
--- recipes-tests/src/test/java/io/quarkus/updates/camel/CamelUpdate42Test.java
+++ recipes-tests/src/test/java/io/quarkus/updates/camel/CamelUpdate42Test.java
@@ -1,13 +1,18 @@
 package io.quarkus.updates.camel;
 
-import org.junit.jupiter.api.Test;
-import org.openrewrite.java.JavaParser;
-import org.openrewrite.properties.Assertions;
 import org.openrewrite.test.RecipeSpec;
-import org.openrewrite.test.RewriteTest;
 import org.openrewrite.test.TypeValidation;
 
 import static org.openrewrite.java.Assertions.java;
 
 public class CamelUpdate42Test extends org.apache.camel.updates.camel44.CamelUpdate42Test {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        //let the parser be initialized in the camel parent
+        super.defaults(spec);
+        //recipe has to be loaded differently
+        CamelQuarkusTestUtil.recipe3_8(spec)
+                .typeValidationOptions(TypeValidation.none());
+    }
 }
