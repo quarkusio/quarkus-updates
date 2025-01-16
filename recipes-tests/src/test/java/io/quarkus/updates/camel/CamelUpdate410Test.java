902ed03c78050675a11116c5b24e76af2f5aa012
Jiri Ondrusek <ondrusek.jiri@gmail.com>
2025-01-16 17:19:54 +0100

    Bump camel-upgrade-recipes to 4.10.0

--- recipes-tests/src/test/java/io/quarkus/updates/camel/CamelUpdate410Test.java
+++ recipes-tests/src/test/java/io/quarkus/updates/camel/CamelUpdate410Test.java
@@ -1,19 +1,16 @@
 package io.quarkus.updates.camel;
 
-import org.junit.jupiter.api.Test;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.TypeValidation;
 
-import static org.openrewrite.java.Assertions.java;
-
-public class CamelUpdate49Test extends org.apache.camel.upgrade.CamelUpdate49Test {
+public class CamelUpdate410Test extends org.apache.camel.upgrade.CamelUpdate410Test {
 
     @Override
     public void defaults(RecipeSpec spec) {
         //let the parser be initialized in the camel parent
         super.defaults(spec);
         //recipe has to be loaded differently
-        CamelQuarkusTestUtil.recipe3_17(spec)
+        CamelQuarkusTestUtil.recipe3_18(spec)
                 .typeValidationOptions(TypeValidation.none());
     }
 }
